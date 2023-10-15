50d337e8438d1d89406e09340ccd7aed789c1458
MatU <mattves@bk.ru>
2023-10-15 14:44:35 +0300

    доработан файл Main.java

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,16 +1,40 @@
 package Main;
 
 import MyFile.MyFile;
-import MyTime.MyTime;
+
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         MyFile file = new MyFile();
         file.CreateFile();
-        file.WriteFile();
-        file.ReadFile();
-        
-
+        while (true) {
+            System.out.println("Введите команду:");
+            String message = scanner.nextLine().toLowerCase();
+            if (message.equals("q")) break;
+            switch (message) {
+                case "add":
+                    System.out.println("Введите запись для сохраниия в файл: ");
+                    String messageInFile = scanner.nextLine();
+                    file.WriteFile(messageInFile);
+                    System.out.println("Запись добавлена в файл");
+                    break;
+                case "read":
+                    file.ReadFile();
+                    break;
+                case "help":
+                    System.out.println("add - добавление записи в файл \n" +
+                            "read - чтение записей из файла \n" +
+                            "q - вызод из программы ");
+                    break;
+                default:
+                    System.out.println("Команда не распознана, повторите запрос или введите help для " +
+                            "уточнения возможных команд");
+            }
+        }
+        System.out.println("программа завершена");
+        scanner.close();
     }
 }
--- src/main/java/MyFile/MyFile.java
+++ src/main/java/MyFile/MyFile.java
@@ -6,36 +6,39 @@ import java.io.*;
 import java.util.Scanner;
 
 public class MyFile {
-    public void CreateFile(){
-        try{
-            File file = new File("D:\\File.txt");
-            if(file.createNewFile()){
-                System.out.println("File created");
-            }
-            else System.out.println("File has already been created");
+    //Место создания файла
+    String puthFile = "D:\\File.txt";
+
+    public void CreateFile() {
+        try {
+            File file = new File(puthFile);
+            if (file.createNewFile()) {
+                System.out.println("Файл создан в " + puthFile);
+            } else System.out.println("Файл уже создан в " + puthFile);
 
         } catch (Exception e) {
             System.err.println(e);
         }
     }
 
-    public void WriteFile(){
-        File myfile = new File("D:\\File.txt");
+    public void WriteFile(String messageInFile) {
+        File myfile = new File(puthFile);
         MyTime myTime = new MyTime();
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(myfile,true));
-            writer.write((myTime.getTime()) + " -> text \n");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(myfile, true));
+            writer.write((myTime.getTime()) + " -> " + messageInFile + "\n");
             writer.close();
         } catch (IOException e) {
             System.err.println(e);
         }
     }
-    public void ReadFile(){
+
+    public void ReadFile() {
 
         try {
-            FileReader reader = new FileReader("D:\\File.txt");
+            FileReader reader = new FileReader(puthFile);
             Scanner scan = new Scanner(reader);
-            while (scan.hasNextLine()){
+            while (scan.hasNextLine()) {
                 System.out.println(scan.nextLine());
             }
             scan.close();
